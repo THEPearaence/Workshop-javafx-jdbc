c7600f1dc5cdc351111c4700673616d9ee86cd3e
THEParaence <128623447+THEPearaence@users.noreply.github.com>
2024-04-18 14:38:07 -0300

    DepartmentList view design

--- WjavafxJDBC/src/gui/MainViewController.java
+++ WjavafxJDBC/src/gui/MainViewController.java
@@ -34,7 +34,7 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		System.out.println("Funcinou o OMIDA");
+		loadView("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
