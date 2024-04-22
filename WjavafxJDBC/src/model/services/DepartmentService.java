ad04527979e7d1aadc9cf682fe80820898a16f10
THEParaence <128623447+THEPearaence@users.noreply.github.com>
2024-04-22 12:04:38 -0300

    Adding database access

--- WjavafxJDBC/src/model/services/DepartmentService.java
+++ WjavafxJDBC/src/model/services/DepartmentService.java
@@ -1,19 +1,18 @@
 package model.services;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+	
 	public List<Department> findAll(){
-		List<Department> list = new ArrayList<>();
-		list.add(new Department(1,"Book"));
-		list.add(new Department(2,"Computer"));
-		list.add(new Department(3,"Eletronic"));
-		return list;
+		return dao.findAll();
 	}
 }
